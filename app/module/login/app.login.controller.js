bf876355ee799dcf8a8e28b97e1b734f6f02f927
zahir <hamroune.z@gmail.com>
2015-08-08 15:08:20 +0200

    add Replication, Sync

--- app/module/login/app.login.controller.js
+++ app/module/login/app.login.controller.js
@@ -6,23 +6,24 @@
 
         $rootScope.user ={};
 
+        $rootScope.sync = function(dbname){
+          var localDB = new PouchDB(dbname);
+          var url = $rootScope.BASE_URL+dbname;
+          var remoteDb = new PouchDB(url);
+          localDB.sync(remoteDb);
+
+        }
+
         $rootScope.login=function(){
 
           console.log('$rootScope.username', $rootScope.user.username)
 
-            $rootScope.BASE_URL_AUTH = 'http://195.154.223.114:5984/'
-               
-           
-            var db = new PouchDB($rootScope.BASE_URL_AUTH+"_users");
+            $rootScope.BASE_URL_AUTH = 'http://195.154.223.114:5984/test'
+         
+            var db = new PouchDB($rootScope.BASE_URL_AUTH);
+            
 
             db.login($rootScope.user.username, $scope.user.password , function (err,response) {
-
-                 console.log('response ===>', response, 'err', err);
-
-               $rootScope.BASE_URL = 'http://'+$rootScope.user.username+':'+$scope.user.password+'@195.154.223.114:5984/'
-                window.SyncloudUrl = $rootScope.BASE_URL;
-
-
               if (err) {
                 if (err.name === 'unauthorized') {
                   $scope.error="Userame or Password incorrect";                }
@@ -31,8 +32,19 @@
                 }
               }
               else{
-                $location.path("/home");
+
+                console.log('Athenticated ', response);
+                $rootScope.BASE_URL = 'http://'+$rootScope.user.username+':'+$scope.user.password+'@195.154.223.114:5984/';
+                
+                $rootScope.sync('users_replicat');
+                $rootScope.sync('applications');
+
+                $state.go("home");
               }
+
+
+
+              
           });
 
         }
